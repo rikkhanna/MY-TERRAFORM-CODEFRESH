1245dae7ff47aa77448b82c5d69a61f5fe39d90f
unknown <rishabhkhanna726@gmail.com>
2022-09-14 23:44:46 +0530

    files modified

--- terraform-codefresh-project/_provider.tf
+++ terraform-codefresh-project/_provider.tf
@@ -1,3 +1,12 @@
+terraform {
+
+  required_providers {
+    codefresh = {
+      source = "registry.terraform.io/codefresh-io/codefresh"
+    }
+  }
+}
+
 provider "codefresh" {
   api_url   =  var.codefresh_uri
   token = var.codefresh_token
